d5ad7acc5f27ea53e3d4b86f6fe688555f1e057f
mkess-ivy <montier.kess@gmail.com>
2017-10-24 11:59:29 -0400

    desktop home layout finished, need to test bigger screens

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,24 +1,21 @@
-jQuery(document).ready(function (e) {
-    function t(t) {
-        e(t).bind("click", function (t) {
-            t.preventDefault();
-            e(this).parent().fadeOut()
-        })
-    }
-    e(".dropdown-toggle").click(function () {
-        var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
-        e(".button-dropdown .dropdown-menu").hide();
-        e(".button-dropdown .dropdown-toggle").removeClass("active");
-        if (t) {
-            e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
-        }
-    });
-    e(document).bind("click", function (t) {
-        var n = e(t.target);
-        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
-    });
-    e(document).bind("click", function (t) {
-        var n = e(t.target);
-        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
-    })
+$(document).ready(function() {
+  
+  // Slick Slider + Properties
+  $('.primary_slider').slick({
+    autoplay: true,
+    arrows: false,
+    fade: true,
+    speed: 500,
+    cssEase: 'linear'
+  });
+
+  // Catalog Hover Feature
+  $(".js-background-trigger").hover(function(){
+   	$(".js-background-receiver", this).addClass('hover');
+  }
+  , 
+  function(){
+    $(".js-background-receiver", this).removeClass('hover');
+  });
+
 });
